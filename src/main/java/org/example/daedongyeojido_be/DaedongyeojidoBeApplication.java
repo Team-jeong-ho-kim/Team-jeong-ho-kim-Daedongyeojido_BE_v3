b7a0c6065c88154c3a094e8dbdc4c9c25ffd36b8
Hannah <sunday0206@dsm.hs.kr>
2024-09-14 23:25:14 +0900

    chore: 프로젝트 기초세팅

---
+++ src/main/java/org/example/daedongyeojido_be/DaedongyeojidoBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.daedongyeojido_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongyeojidoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaedongyeojidoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/daedongyeojido_be/DaedongyeojidoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.daedongyeojido_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaedongyeojidoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
