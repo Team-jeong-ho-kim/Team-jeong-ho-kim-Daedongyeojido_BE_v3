7a2159ce56cfd168bd4f0977ac794c1d311410e5
박주원 <qkrwndnjs7308@gmail.com>
2024-12-11 22:33:56 +0900

    Modify :: Exception 이름 수정

--- src/main/java/org/example/daedongyeojido_be/domain/file/exception/FailFileException.java
+++ src/main/java/org/example/daedongyeojido_be/domain/file/exception/FailFileException.java
@@ -1,12 +1,12 @@
 package org.example.daedongyeojido_be.domain.file.exception;
 
 
-import org.example.daedongyeojido_be.global.config.error.exception.DayException;
+import org.example.daedongyeojido_be.global.config.error.exception.DaeDongException;
 import org.example.daedongyeojido_be.global.config.error.exception.ErrorCode;
 
-public class FailFileException extends DayException {
+public class FailFileException extends DaeDongException {
 
-    public static final DayException EXCEPTION = new FailFileException();
+    public static final DaeDongException EXCEPTION = new FailFileException();
 
     private FailFileException(){
         super(ErrorCode.FAIL_FILE);
--- src/main/java/org/example/daedongyeojido_be/domain/user/exception/UserNotFoundException.java
+++ src/main/java/org/example/daedongyeojido_be/domain/user/exception/UserNotFoundException.java
@@ -1,12 +1,12 @@
 package org.example.daedongyeojido_be.domain.user.exception;
 
 
-import org.example.daedongyeojido_be.global.config.error.exception.DayException;
+import org.example.daedongyeojido_be.global.config.error.exception.DaeDongException;
 import org.example.daedongyeojido_be.global.config.error.exception.ErrorCode;
 
-public class UserNotFoundException extends DayException {
+public class UserNotFoundException extends DaeDongException {
 
-    public static final DayException EXCEPTION = new UserNotFoundException();
+    public static final DaeDongException EXCEPTION = new UserNotFoundException();
 
     public UserNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
