6f7e4cecc0b0c3fca17c5813c592423b07cc75f0
박주원 <qkrwndnjs7308@gmail.com>
2024-12-11 22:29:03 +0900

    Modify :: import

--- src/main/java/org/example/daedongyeojido_be/domain/file/service/FileUploadService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/file/service/FileUploadService.java
@@ -5,12 +5,10 @@ import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Operations;
 import lombok.RequiredArgsConstructor;
 import org.example.daedongyeojido_be.infrastructure.s3.AwsS3Properties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+
 
 @Component
 @RequiredArgsConstructor
@@ -22,7 +20,7 @@ class FileUploadService {
     private final AwsS3Properties s3Properties;
 
     void uploadFile(MultipartFile multipartFile, String key) {
-            // S3 파일 업로드 처리
+
             s3Operations.upload(
                     s3Properties.bucket(),
                     key,
