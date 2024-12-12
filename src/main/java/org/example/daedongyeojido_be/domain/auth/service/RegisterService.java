59507f48a92939f3f4009ec7cc4bd0b6304dbd98
박주원 <qkrwndnjs7308@gmail.com>
2024-12-12 22:24:02 +0900

    add :: service 회원가입

---
+++ src/main/java/org/example/daedongyeojido_be/domain/auth/service/RegisterService.java
@@ -0,0 +1,38 @@
+package org.example.daedongyeojido_be.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.daedongyeojido_be.domain.auth.exception.UserAlreadyTaken;
+import org.example.daedongyeojido_be.domain.auth.presentation.dto.request.RegisterRequest;
+import org.example.daedongyeojido_be.domain.user.domain.User;
+import org.example.daedongyeojido_be.domain.user.repository.UserRepository;
+import org.example.daedongyeojido_be.global.config.security.auth.AuthDetailsService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RegisterService {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+
+    public void register(RegisterRequest request){
+        if(userRepository.existsByUsername(request.username())){
+            throw UserAlreadyTaken.EXCEPTION;
+        }
+
+        userRepository.save(
+                User.builder()
+                        .email(request.email())
+                        .username(request.username())
+                        .password(passwordEncoder.encode(request.password()))
+                        .name(request.name())
+                        .introduce(request.introduce())
+                        .build());
+
+    }
+}
