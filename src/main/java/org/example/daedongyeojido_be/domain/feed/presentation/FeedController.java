61f01473c0503be503e777eb3060910e3c50c2d3
민수아 <Minsua0503@gmail.com>
2024-12-14 16:48:46 +0900

    add :: FeedController

---
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/presentation/FeedController.java
@@ -0,0 +1,53 @@
+package org.example.daedongyeojido_be.domain.feed.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
+import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedDetailResponse;
+import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedListResponse;
+import org.example.daedongyeojido_be.domain.feed.service.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final CreateFeedService createFeedService;
+    private final UpdateFeedService updateFeedService;
+    private final DeleteFeedService deleteFeedService;
+    private final QueryMyAllFeedsService queryMyAllFeedsService;
+    private final QueryFeedDetailService queryFeedDetailService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createFeed(@RequestBody @Valid FeedRequest request) {
+        createFeedService.createFeed(request);
+    }
+
+    @PatchMapping("/{feed-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateFeed(@PathVariable("feed-id") Long feedId, @RequestBody @Valid FeedRequest request) {
+        updateFeedService.updateFeed(feedId, request);
+    }
+
+    @DeleteMapping("/{feed-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFeed(@PathVariable("feed-id") Long feedId) {
+        deleteFeedService.deleteFeed(feedId);
+    }
+
+    @GetMapping
+    public List<FeedListResponse> getMyPosts() {
+        return queryMyAllFeedsService.getAllMyFeeds();
+    }
+
+    @GetMapping("/{feed-id}")
+    public FeedDetailResponse getFeed(@PathVariable("feed-id") Long feedId) {
+        return queryFeedDetailService.getFeedDetail(feedId);
+    }
+
+}
--- src/main/java/org/example/daedongyeojido_be/global/config/config/SecurityConfig.java
+++ src/main/java/org/example/daedongyeojido_be/global/config/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package org.example.daedongyeojido_be.global.config.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.example.daedongyeojido_be.global.config.security.jwt.JwtTokenFilter;
 import org.example.daedongyeojido_be.global.config.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -59,6 +61,7 @@ public class SecurityConfig  {
                         .requestMatchers("/user/update","/user/delete/","/user/my-info").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         // "/admin"이라는 경로는 역할이 "ADMIN"인 사람만 접근할 수 있다.
+                        .requestMatchers("/feed").permitAll()
                         .anyRequest().authenticated());
         // 다른 모든 요청은 인증된 사용자만 접근할 수 있다.
 
@@ -68,6 +71,7 @@ public class SecurityConfig  {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // 세션 : STATELESS, 비활성화 시킴
 
+        http.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
         // 필터체인에 결과를 반환
