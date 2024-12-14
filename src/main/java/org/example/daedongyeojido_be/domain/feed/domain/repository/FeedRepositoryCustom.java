e707b489ad0a521be0bbeec97c4c8441aa0d7402
민수아 <Minsua0503@gmail.com>
2024-12-15 02:43:50 +0900

    add :: Repository에 임시저장 기능 추가

--- src/main/java/org/example/daedongyeojido_be/domain/feed/domain/repository/FeedRepositoryCustom.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/domain/repository/FeedRepositoryCustom.java
@@ -2,6 +2,7 @@ package org.example.daedongyeojido_be.domain.feed.domain.repository;
 
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedDetailResponse;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedListResponse;
+import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.TemporaryFeedListResponse;
 
 import java.util.List;
 
@@ -9,6 +10,6 @@ public interface FeedRepositoryCustom {
 
     List<FeedListResponse> findMyFeedsByUserId(Long userId);
     FeedDetailResponse findByFeedId(Long feedId);
-    List<FeedListResponse> findTemporaryFeedsByUserId(Long userId);
+    List<TemporaryFeedListResponse> findTemporaryFeedsByUserId(Long userId);
 
 }
