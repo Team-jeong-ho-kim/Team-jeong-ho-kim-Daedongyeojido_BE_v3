5764f39a99b7e261e673187109ac35024c3594c4
민수아 <Minsua0503@gmail.com>
2024-12-14 16:56:10 +0900

    add :: FeedService
    임시저장을 제외한 피드 생성, 삭제, 수정 api입니다

---
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/service/UpdateFeedService.java
@@ -0,0 +1,32 @@
+package org.example.daedongyeojido_be.domain.feed.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.daedongyeojido_be.domain.feed.domain.Feed;
+import org.example.daedongyeojido_be.domain.feed.exception.CannotModifyFeedException;
+import org.example.daedongyeojido_be.domain.feed.facade.FeedFacade;
+import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
+import org.example.daedongyeojido_be.domain.user.domain.User;
+import org.example.daedongyeojido_be.domain.user.facade.UserFacade;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateFeedService {
+
+    private final UserFacade userFacade;
+    private final FeedFacade feedFacade;
+
+    @Transactional
+    public void updateFeed(Long feedId, FeedRequest request) {
+        User user = userFacade.currentUser();
+        Feed feed = feedFacade.getFeed(feedId);
+
+        if(!user.getId().equals(feed.getUser().getId())) {
+            throw CannotModifyFeedException.EXCEPTION;
+        }
+
+        feed.updateFeed(request.getTitle(), request.getContent(), request.getPreviewImageUrl(), request.getIntroduction(), request.getUrl(), request.isPublished());
+    }
+
+}
