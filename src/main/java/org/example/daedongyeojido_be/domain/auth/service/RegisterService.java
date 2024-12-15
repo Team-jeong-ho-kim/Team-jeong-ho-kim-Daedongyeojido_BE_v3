5bea7410aecb7e0bfcb4e1aa25fc9d34c2356f52
박주원 <qkrwndnjs7308@gmail.com>
2024-12-15 16:32:01 +0900

    fix :: 필드 수정

--- src/main/java/org/example/daedongyeojido_be/domain/auth/service/RegisterService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/auth/service/RegisterService.java
@@ -3,6 +3,7 @@ package org.example.daedongyeojido_be.domain.auth.service;
 import lombok.RequiredArgsConstructor;
 import org.example.daedongyeojido_be.domain.auth.exception.UserAlreadyTaken;
 import org.example.daedongyeojido_be.domain.auth.presentation.dto.request.RegisterRequest;
+import org.example.daedongyeojido_be.domain.file.domain.DefaultImageProperties;
 import org.example.daedongyeojido_be.domain.user.domain.User;
 import org.example.daedongyeojido_be.domain.user.repository.UserRepository;
 import org.example.daedongyeojido_be.global.config.security.auth.AuthDetailsService;
@@ -17,6 +18,7 @@ public class RegisterService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final DefaultImageProperties defaultImageProperties;
 
 
 
@@ -32,6 +34,7 @@ public class RegisterService {
                         .password(passwordEncoder.encode(request.password()))
                         .name(request.name())
                         .introduce(request.introduce())
+                        .profileImageUrl(defaultImageProperties.ImageUrl())
                         .build());
 
     }
