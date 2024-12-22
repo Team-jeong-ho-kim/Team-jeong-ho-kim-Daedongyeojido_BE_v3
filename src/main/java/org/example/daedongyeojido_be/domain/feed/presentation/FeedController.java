e437014eb8fc66d2ced96fd6ba3b9ed366b38dbc
민수아 <Minsua0503@gmail.com>
2024-12-22 21:32:18 +0900

    fix :: feed controller 수정

--- src/main/java/org/example/daedongyeojido_be/domain/feed/presentation/FeedController.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/presentation/FeedController.java
@@ -1,13 +1,10 @@
 package org.example.daedongyeojido_be.domain.feed.presentation;
 
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.example.daedongyeojido_be.domain.feed.domain.Feed;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedDetailResponse;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.FeedListResponse;
-import org.example.daedongyeojido_be.domain.feed.presentation.dto.response.TemporaryFeedListResponse;
 import org.example.daedongyeojido_be.domain.feed.service.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +19,7 @@ public class FeedController {
     private final CreateFeedService createFeedService;
     private final UpdateFeedService updateFeedService;
     private final DeleteFeedService deleteFeedService;
-    private final QueryMyAllFeedsService queryMyAllFeedsService;
     private final QueryFeedDetailService queryFeedDetailService;
-    private final TemporarySaveFeedService temporarySaveFeedService;
-    private final QueryTemporarySaveFeedsService queryTemporarySaveFeedsService;
     private final QueryFeedListService queryFeedListService;
 
     @PostMapping
@@ -46,28 +40,12 @@ public class FeedController {
         deleteFeedService.deleteFeed(feedId);
     }
 
-    @GetMapping("my")
-    public List<FeedListResponse> getMyPosts() {
-        return queryMyAllFeedsService.getAllMyFeeds();
-    }
-
     @GetMapping("/{feed-url}")
     public FeedDetailResponse getFeed(@PathVariable("feed-url") String feedUrl) {
         return queryFeedDetailService.getFeedDetail(feedUrl);
     }
 
-    @PostMapping("/temporary")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void temporarySave(@RequestBody FeedRequest request) {
-        temporarySaveFeedService.temporarySave(request);
-    }
-
-    @GetMapping("/temporary")
-    public List<TemporaryFeedListResponse> temporaryList() {
-        return queryTemporarySaveFeedsService.getTemporaryFeeds();
-    }
-
-    @GetMapping()
+    @GetMapping
     public List<FeedListResponse> getFeedList() {
         return queryFeedListService.getFeedList();
     }
