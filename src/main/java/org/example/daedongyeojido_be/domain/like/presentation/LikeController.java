ad91fb352ff2608920300cfc4fc8985f6be800fe
민수아 <Minsua0503@gmail.com>
2024-12-22 21:30:37 +0900

    fix :: 좋아요 서비스 주석처리

--- src/main/java/org/example/daedongyeojido_be/domain/like/presentation/LikeController.java
+++ src/main/java/org/example/daedongyeojido_be/domain/like/presentation/LikeController.java
@@ -1,26 +1,26 @@
-package org.example.daedongyeojido_be.domain.like.presentation;
-
-import lombok.RequiredArgsConstructor;
-import org.example.daedongyeojido_be.domain.like.service.DeleteLikeService;
-import org.example.daedongyeojido_be.domain.like.service.LikeService;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/like")
-@RequiredArgsConstructor
-public class LikeController {
-
-    private final LikeService likeService;
-    private final DeleteLikeService deleteLikeService;
-
-    @PostMapping("/{feed-id}")
-    public void likeFeed(@PathVariable("feed-id") Long feedId) {
-        likeService.liked(feedId);
-    }
-
-    @DeleteMapping("/{feed-id}")
-    public void deleteFeed(@PathVariable("feed-id") Long feedId) {
-        deleteLikeService.deleteLike(feedId);
-    }
-
-}
+//package org.example.daedongyeojido_be.domain.like.presentation;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.daedongyeojido_be.domain.like.service.DeleteLikeService;
+//import org.example.daedongyeojido_be.domain.like.service.LikeService;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/like")
+//@RequiredArgsConstructor
+//public class LikeController {
+//
+//    private final LikeService likeService;
+//    private final DeleteLikeService deleteLikeService;
+//
+//    @PostMapping("/{feed-id}")
+//    public void likeFeed(@PathVariable("feed-id") Long feedId) {
+//        likeService.liked(feedId);
+//    }
+//
+//    @DeleteMapping("/{feed-id}")
+//    public void deleteFeed(@PathVariable("feed-id") Long feedId) {
+//        deleteLikeService.deleteLike(feedId);
+//    }
+//
+//}
--- src/main/java/org/example/daedongyeojido_be/domain/like/service/DeleteLikeService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/like/service/DeleteLikeService.java
@@ -1,36 +1,36 @@
-package org.example.daedongyeojido_be.domain.like.service;
-
-import lombok.RequiredArgsConstructor;
-import org.example.daedongyeojido_be.domain.feed.domain.Feed;
-import org.example.daedongyeojido_be.domain.feed.facade.FeedFacade;
-import org.example.daedongyeojido_be.domain.like.domain.repository.LikeRepository;
-import org.example.daedongyeojido_be.domain.like.exception.CannotDeleteLikeException;
-import org.example.daedongyeojido_be.domain.like.facade.LikeFacade;
-import org.example.daedongyeojido_be.domain.user.domain.User;
-import org.example.daedongyeojido_be.domain.user.facade.UserFacade;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@RequiredArgsConstructor
-public class DeleteLikeService {
-
-    private final LikeRepository likeRepository;
-    private final UserFacade userFacade;
-    private final FeedFacade feedFacade;
-    private final LikeFacade likeFacade;
-
-    @Transactional
-    public void deleteLike(Long feedId) {
-        User user = userFacade.currentUser();
-        Feed feed = feedFacade.getFeed(feedId);
-
-        if(!likeFacade.checkLiked(user, feed)) {
-            throw CannotDeleteLikeException.EXCEPTION;
-        }
-
-        feed.minusLike();
-        likeRepository.deleteLikeByUserAndFeed(user, feed);
-    }
-
-}
+//package org.example.daedongyeojido_be.domain.like.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.daedongyeojido_be.domain.feed.domain.Feed;
+//import org.example.daedongyeojido_be.domain.feed.facade.FeedFacade;
+//import org.example.daedongyeojido_be.domain.like.domain.repository.LikeRepository;
+//import org.example.daedongyeojido_be.domain.like.exception.CannotDeleteLikeException;
+//import org.example.daedongyeojido_be.domain.like.facade.LikeFacade;
+//import org.example.daedongyeojido_be.domain.user.domain.User;
+//import org.example.daedongyeojido_be.domain.user.facade.UserFacade;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class DeleteLikeService {
+//
+//    private final LikeRepository likeRepository;
+//    private final UserFacade userFacade;
+//    private final FeedFacade feedFacade;
+//    private final LikeFacade likeFacade;
+//
+//    @Transactional
+//    public void deleteLike(Long feedId) {
+//        User user = userFacade.currentUser();
+//        Feed feed = feedFacade.getFeed(feedId);
+//
+//        if(!likeFacade.checkLiked(user, feed)) {
+//            throw CannotDeleteLikeException.EXCEPTION;
+//        }
+//
+//        feed.minusLike();
+//        likeRepository.deleteLikeByUserAndFeed(user, feed);
+//    }
+//
+//}
