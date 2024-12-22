c2f07a1985430edde72cab6c7a7c76510ff99085
민수아 <Minsua0503@gmail.com>
2024-12-22 21:31:56 +0900

    fix :: 불필요한 서비스 코드 주석처리

--- src/main/java/org/example/daedongyeojido_be/domain/feed/service/TemporarySaveFeedService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/service/TemporarySaveFeedService.java
@@ -1,33 +1,33 @@
-package org.example.daedongyeojido_be.domain.feed.service;
-
-import lombok.RequiredArgsConstructor;
-import org.example.daedongyeojido_be.domain.feed.domain.Feed;
-import org.example.daedongyeojido_be.domain.feed.domain.repository.FeedRepository;
-import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
-import org.example.daedongyeojido_be.domain.user.domain.User;
-import org.example.daedongyeojido_be.domain.user.facade.UserFacade;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@RequiredArgsConstructor
-public class TemporarySaveFeedService {
-
-    private final UserFacade userFacade;
-    private final FeedRepository feedRepository;
-
-    @Transactional
-    public void temporarySave(FeedRequest request) {
-        User user = userFacade.currentUser();
-
-        feedRepository.save(Feed.builder()
-                .title(request.getTitle())
-                .content(request.getContent())
-                .introduction(request.getContent())
-                .feedUrl(request.getTitle())
-                .isPublished(false)
-                .user(user)
-                .build());
-    }
-
-}
+//package org.example.daedongyeojido_be.domain.feed.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.daedongyeojido_be.domain.feed.domain.Feed;
+//import org.example.daedongyeojido_be.domain.feed.domain.repository.FeedRepository;
+//import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
+//import org.example.daedongyeojido_be.domain.user.domain.User;
+//import org.example.daedongyeojido_be.domain.user.facade.UserFacade;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class TemporarySaveFeedService {
+//
+//    private final UserFacade userFacade;
+//    private final FeedRepository feedRepository;
+//
+//    @Transactional
+//    public void temporarySave(FeedRequest request) {
+//        User user = userFacade.currentUser();
+//
+//        feedRepository.save(Feed.builder()
+//                .title(request.getTitle())
+//                .content(request.getContent())
+//                .introduction(request.getContent())
+//                .feedUrl(request.getTitle())
+//                .isPublished(false)
+//                .user(user)
+//                .build());
+//    }
+//
+//}
