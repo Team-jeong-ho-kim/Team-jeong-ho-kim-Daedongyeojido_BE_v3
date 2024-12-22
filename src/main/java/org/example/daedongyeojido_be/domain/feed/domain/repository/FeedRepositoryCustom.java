80c8b386faec60da6388b12230f720260ca79b04
민수아 <Minsua0503@gmail.com>
2024-12-22 21:30:18 +0900

    fix :: feed repository 불필요한 코드 주석처리

--- src/main/java/org/example/daedongyeojido_be/domain/feed/domain/repository/FeedRepositoryCustom.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/domain/repository/FeedRepositoryCustom.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface FeedRepositoryCustom {
 
-    List<FeedListResponse> findMyFeedsByUserId(Long userId);
+    //List<FeedListResponse> findMyFeedsByUserId(Long userId);
     FeedDetailResponse findByFeedUrl(String feedUrl);
-    List<TemporaryFeedListResponse> findTemporaryFeedsByUserId(Long userId);
+    //List<TemporaryFeedListResponse> findTemporaryFeedsByUserId(Long userId);
     List<FeedListResponse> getFeeds();
 
 }
