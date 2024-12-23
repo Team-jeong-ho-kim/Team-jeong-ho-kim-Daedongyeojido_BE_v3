7197b6d017986334527f0acb899e1a42a7bb60ec
민수아 <Minsua0503@gmail.com>
2024-12-23 16:12:04 +0900

    add :: FeedService 사진기능 추가

--- src/main/java/org/example/daedongyeojido_be/domain/feed/service/CreateFeedService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/service/CreateFeedService.java
@@ -4,23 +4,41 @@ import lombok.RequiredArgsConstructor;
 import org.example.daedongyeojido_be.domain.feed.domain.Feed;
 import org.example.daedongyeojido_be.domain.feed.domain.repository.FeedRepository;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
+import org.example.daedongyeojido_be.domain.file.domain.type.ImageType;
+import org.example.daedongyeojido_be.domain.file.exception.FailFileException;
+import org.example.daedongyeojido_be.domain.file.service.ImageService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class CreateFeedService {
 
     private final FeedRepository feedRepository;
+    private final ImageService imageService;
 
     @Transactional
-    public void createFeed(FeedRequest request) {
+    public void createFeed(FeedRequest request, MultipartFile file) {
+        String previewImageUrl = null;
+
+        if (file != null && !file.isEmpty()) {
+            try {
+                String key = imageService.saveImage(file, ImageType.PREVIEW_IMAGE_FOLDER);
+                previewImageUrl = imageService.getFileBaseUrl() + key;
+            } catch (IOException e) {
+                throw FailFileException.EXCEPTION;
+            }
+        }
 
         feedRepository.save(Feed.builder()
                         .title(request.getTitle())
                         .content(request.getContent())
                         .userName(request.getUserName())
                         .createdAt(request.getCreatedAt())
+                        .previewImageUrl(previewImageUrl)
                 .build());
     }
 
