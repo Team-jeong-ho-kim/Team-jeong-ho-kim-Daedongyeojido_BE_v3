0b3c6f215de0e17d915594c49fcaaa51ba898251
troy96475 <taesupark0504@gmail.com>
2024-12-26 16:36:58 +0900

    refactor :: update로직 수정

--- src/main/java/org/example/daedongyeojido_be/domain/feed/service/UpdateFeedService.java
+++ src/main/java/org/example/daedongyeojido_be/domain/feed/service/UpdateFeedService.java
@@ -4,19 +4,39 @@ import lombok.RequiredArgsConstructor;
 import org.example.daedongyeojido_be.domain.feed.domain.Feed;
 import org.example.daedongyeojido_be.domain.feed.facade.FeedFacade;
 import org.example.daedongyeojido_be.domain.feed.presentation.dto.request.FeedRequest;
+import org.example.daedongyeojido_be.domain.file.domain.type.ImageType;
+import org.example.daedongyeojido_be.domain.file.exception.FailFileException;
+import org.example.daedongyeojido_be.domain.file.service.ImageService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateFeedService {
 
     private final FeedFacade feedFacade;
+    private final ImageService imageService;
 
     @Transactional
-    public void updateFeed(Long feedId, FeedRequest request) {
+    public void updateFeed(Long feedId, FeedRequest request, MultipartFile file) {
         Feed feed = feedFacade.getFeed(feedId);
-        feed.updateFeed(request.getTitle(), request.getContent(), request.getUserName(), request.getCreatedAt());
+
+        if (file != null && !file.isEmpty()) {
+            try {
+                String key = imageService.saveImage(file, ImageType.PREVIEW_IMAGE_FOLDER);
+
+                String previewImageUrl = imageService.getFileBaseUrl() + key;
+
+                feed.setPreviewImageUrl(previewImageUrl);
+            } catch (IOException e) {
+                throw FailFileException.EXCEPTION;
+            }
+        }
+
+        feed.updateFeed(request.getTitle(), request.getContent(), request.getUserName(), request.getCreatedAt(), request.getPreviewImageUrl());
     }
 
 }
